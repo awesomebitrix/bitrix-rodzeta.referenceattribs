0cbb136de07e89b4535fb134149093e59f2b5533
Rick Deckard <xxxblasteroid@gmail.com>
2016-07-13 08:33:08 +0300

    fix

---
+++ lib/filter.php
@@ -0,0 +1,21 @@
+<?php
+/***********************************************************************************************
+ * rodzeta.referenceattribs - Infoblock element reference attributes
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ************************************************************************************************/
+
+namespace Rodzeta\Referenceattribs;
+
+use \Bitrix\Main\Application;
+use \Bitrix\Main\Config\Option;
+
+final class Filter {
+
+	static function getIds($params, $iblockId = null) {
+		$result = array();
+
+		return $result;
+	}
+
+}
