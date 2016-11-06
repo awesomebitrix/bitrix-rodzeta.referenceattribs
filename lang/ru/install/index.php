aa62f364a2541222acf526100c35b79bd1b68d1f
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-06 09:49:03 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,4 +2,4 @@
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
 $MESS["RODZETA_REFERENCEATTRIBS_MODULE_NAME"] = "Справочники для элемента инфоблока на основе разделов";
-$MESS["RODZETA_REFERENCEATTRIBS_MODULE_DESCRIPTION"] = "Позволяет реализовать свойства-справочники для элемента инфоблока на основе разделов";
+$MESS["RODZETA_REFERENCEATTRIBS_MODULE_DESCRIPTION"] = "Данный модуль представляет собой конструктор атрибутов, который позволяет реализовать характеристики элемента (например товаров) на базе стандартных разделов инфоблоков, а так же использовать данные параметры для реализации ЧПУ-фильтра.";
