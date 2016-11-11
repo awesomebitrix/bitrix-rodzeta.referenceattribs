7ccc02e168673efd27af2496382c1eaf64c88393
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-11 11:19:58 +0300

    fix

--- include.php
+++ include.php
@@ -1,9 +1,9 @@
 <?php
-/***********************************************************************************************
+/*******************************************************************************
  * rodzeta.referenceattribs - Infoblock element reference attributes
  * Copyright 2016 Semenov Roman
  * MIT License
- ************************************************************************************************/
+ ******************************************************************************/
 
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
@@ -11,4 +11,5 @@ require __DIR__ . "/.init.php";
 
 use Bitrix\Main\Loader;
 
+// TODO check for including
 Loader::includeModule("iblock");
--- install/index.php
+++ install/index.php
@@ -1,9 +1,11 @@
 <?php
-/***********************************************************************************************
+/*******************************************************************************
  * rodzeta.referenceattribs - Infoblock element reference attributes
  * Copyright 2016 Semenov Roman
  * MIT License
- ************************************************************************************************/
+ ******************************************************************************/
+
+// NOTE this file must compatible with php 5.3
 
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
@@ -16,7 +18,7 @@ Loc::loadMessages(__FILE__);
 
 class rodzeta_referenceattribs extends CModule {
 
-	var $MODULE_ID = "rodzeta.referenceattribs"; // FIX for bitrix rules
+	var $MODULE_ID = "rodzeta.referenceattribs"; // NOTE using "var" for bitrix rules
 
 	public $MODULE_VERSION;
 	public $MODULE_VERSION_DATE;
@@ -31,7 +33,7 @@ class rodzeta_referenceattribs extends CModule {
 	//public $NEED_MODULES = array();
 
 	function __construct() {
-		$this->MODULE_ID = "rodzeta.referenceattribs"; // NEED for showing module in /bitrix/admin/partner_modules.php?lang=ru
+		$this->MODULE_ID = "rodzeta.referenceattribs"; // NOTE for showing module in /bitrix/admin/partner_modules.php?lang=ru
 
 		$arModuleVersion = array();
 		include __DIR__ . "/version.php";
@@ -55,11 +57,19 @@ class rodzeta_referenceattribs extends CModule {
 	    $_SERVER["DOCUMENT_ROOT"] . "/bitrix/admin/" . $this->MODULE_ID,
 	    true, true
     );
+
     CopyDirFiles(
 	    $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/" . $this->MODULE_ID . "/install/components/rodzeta/catalog.filtersef",
 	    $_SERVER["DOCUMENT_ROOT"] . "/bitrix/components/rodzeta/catalog.filtersef",
 	    true, true
     );
+
+    // copy example if not exists
+		$fname = $_SERVER["DOCUMENT_ROOT"] . "/upload/." . $this->MODULE_ID . ".php";
+		if (!file_exists($fname)) {
+			copy($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/" . $this->MODULE_ID . "/install/data/." . $this->MODULE_ID . ".php", $fname);
+		}
+
 		return true;
 	}
 
@@ -70,6 +80,12 @@ class rodzeta_referenceattribs extends CModule {
 	}
 
 	function DoInstall() {
+		if (version_compare(PHP_VERSION, '7', '<')) {
+			global $APPLICATION;
+   		$APPLICATION->ThrowException(Loc::getMessage("RODZETA_REQUIREMENTS_PHP_VERSION"));
+			return false;
+		}
+
 		ModuleManager::registerModule($this->MODULE_ID);
 		RegisterModuleDependences("main", "OnPageStart", $this->MODULE_ID);
 		$this->InstallFiles();
