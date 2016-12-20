54f67fe080478b6dbf56172794de3c29e06e8732
Rick Deckard <xxxblasteroid@gmail.com>
2016-12-20 17:40:26 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,7 +1,7 @@
 <?php
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
-$MESS["RODZETA_REFERENCEATTRIBS_MODULE_NAME"] = "Справочники для элемента инфоблока на основе разделов";
+$MESS["RODZETA_REFERENCEATTRIBS_MODULE_NAME"] = "Свойства-справочники для ЧПУ-фильтра";
 $MESS["RODZETA_REFERENCEATTRIBS_MODULE_DESCRIPTION"] = "Данный модуль представляет собой конструктор атрибутов, который позволяет реализовать характеристики элемента (например товаров) на базе стандартных разделов инфоблоков, а так же использовать данные параметры для реализации ЧПУ-фильтра.";
 
 $MESS["RODZETA_REQUIREMENTS_PHP_VERSION"] = "Необходима версия PHP 7 или выше";
